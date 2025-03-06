331da275b363e821257c0dcf656bc633795ee6b2
Hp <adkhamjon.abd@gmail.com>
2025-03-06 12:25:08 +0100

    Initial commit: Spring Boot Blog Management System setup

---
+++ src/main/java/com/adam/blog/blogmanagement/BlogManagementApplication.java
@@ -0,0 +1,14 @@
+package com.adam.blog.blogmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogManagementApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
