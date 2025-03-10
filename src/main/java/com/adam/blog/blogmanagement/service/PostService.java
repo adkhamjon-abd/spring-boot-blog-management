c693f6637b943063dda8a7321dcc97166512aca4
Hp <adkhamjon.abd@gmail.com>
2025-03-10 06:27:41 +0100

    improve method signatures and error handling in Post service class

--- src/main/java/com/adam/blog/blogmanagement/service/PostService.java
+++ src/main/java/com/adam/blog/blogmanagement/service/PostService.java
@@ -2,8 +2,11 @@ package com.adam.blog.blogmanagement.service;
 
 import com.adam.blog.blogmanagement.Entity.Post;
 import com.adam.blog.blogmanagement.repository.PostRepository;
+import org.springframework.beans.factory.UnsatisfiedDependencyException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PostService {
@@ -11,17 +14,25 @@ public class PostService {
     @Autowired
     PostRepository postRepository;
 
-    public void createPost(Post post){
+    public Post findById(int postId){
+        return postRepository.findById(postId)
+                .orElseThrow(() -> new IllegalArgumentException("post not found"));
+    }
+    public Post createPost(Post post){
         postRepository.save(post);
+        return post;
     }
 
     public void updatePost(Post post){
-        Post existingPost = postRepository.findById(post.getId()).orElseThrow(() -> new RuntimeException("Post not found"));
+        Post existingPost = postRepository.findById(post.getId()).orElseThrow(() -> new IllegalArgumentException("Post not found"));
         existingPost.setTitle(post.getTitle());
         existingPost.setContent(post.getContent());
         postRepository.save(existingPost);
     }
-    public void deletePost(Post post){
-        postRepository.deleteById(post.getId());
+    public void deletePost(int postId){
+        if(!postRepository.existsById(postId)){
+            throw new IllegalArgumentException("post not found");
+        }
+        postRepository.deleteById(postId);
     }
 }
